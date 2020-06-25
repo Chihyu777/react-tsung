e5f338f39b6ea4869002348b4482eb82e58c0bb4
yusiang <tsungimd106@gmail.com>
2020-06-25 10:42:59 +0800

    首頁畫面大致這樣

--- react-tsung/src/index.js
+++ react-tsung/src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Helmet } from "react-helmet"
+import { Route, HashRouter } from 'react-router-dom';
+import { Container, Row, Col } from "react-bootstrap"
+import routers from './modules/router.js';
+// import "../../bootstrap/dist/css/bootstrap.min.css";
+import "./css/main.css";
+class App extends React.Component {
+    render() {
+        return (
+            <>
+                <Helmet>
+                    <title>國考粽粽</title>
+                </Helmet>
+                <Container>                  
+                    <Row>                       
+                        <Col >
+                        {routers.map((router, index) => (
+                                <Route exact key={index} {...router.routeProps} />
+                            ))}
+                        </Col>
+                    </Row>                    
+                </Container>
+            </>
+        );
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <HashRouter >
+        <App />
+    </HashRouter>,
+    document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
